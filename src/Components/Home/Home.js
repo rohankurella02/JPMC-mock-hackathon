920d39a93315aab9c2a592b5e8956cccac17101b
rohankurella02 <rohankurella02@gmail.com>
2022-06-18 12:43:22 +0530

    Home page Created

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,7 +3,18 @@ import './Home.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <div className="context">
+            <div className="contextHeader">
+                <h1>UNITED WAY OF BENGALURU</h1>
+                <p>A Society For the People By the People</p>
+            </div>
+            <div className="imageContainer">
+            <img src="https://www.uwbengaluru.org/home_banners/headerhome_01_new.jpg" alt="" />
+        </div>
+        </div>
+        
+    </div>
   )
 }
 
